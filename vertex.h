03ed09a968dac0fce8dfefff929a8e6b5af2a9ef
mmiRajib <majhar951.uncc@gmail.com>
2016-05-03 19:39:46 -0400

    Update vertex.h

--- vertex.h
+++ vertex.h
@@ -1,6 +1,3 @@
-//                        ITCS 6114/8114: Algorithms and Data Structures
-// 					      Programming Project 2: Shortest Paths in a Network
-// 					            Md Rajib, mrajib@uncc.edu
 
 #ifndef VERTEX_H
 #define VERTEX_H
